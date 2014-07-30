51ccb252fc276fe4618b78138dd137f4b598f7dc
Luke Bond <luke.n.bond@gmail.com>
2014-07-30 13:15:31 +0100

    Updated example

--- example/example.js
+++ example/example.js
@@ -1,2 +1,8 @@
 var PPQueue = require('../index');
-var ppq = new PPQueue({backendUrl: 'tcp://127.0.0.1:9001', frontendUrl: 'tcp://127.0.0.1:9000'});
+var ppq = new PPQueue({backendUrl: 'tcp://127.0.0.1:9001', frontendUrl: 'tcp://127.0.0.1:9000'})
+  .on('frontend', function (message) {
+    console.log(Date.now() + ' - Proxying worker message through to frontend:', message.toString('utf8'));
+  })
+  .on('backend', function (message) {
+    console.log(Date.now() + ' - Proxying client message through to backend:', message.toString('utf8'));
+  });
